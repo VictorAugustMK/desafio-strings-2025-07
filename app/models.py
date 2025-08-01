5e70943167b3ddf6bbc2dfa3c04b5b8f47e6f872
Victor Augusto <victor.macc12@gmail.com>
2025-08-01 00:57:50 -0300

    feat: width sise

--- app/models.py
+++ app/models.py
@@ -3,4 +3,4 @@ from typing import Optional
 
 class FileRequest(BaseModel):
     path: str
-    width: Optional[int] = Field(40, gt=0, le=120, description="Número de caracteres por linha (1 a 120)")
+    width: Optional[int] = None
